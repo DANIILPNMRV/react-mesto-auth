c80632bc01a11ed4c36395064553df117bd9a2d4
PNMRV <pon.daniil2013@yandex.ru>
2022-12-12 23:54:32 +0300

    wb2

--- src/utils/InitialCards.js
+++ src/utils/InitialCards.js
@@ -1,26 +1,33 @@
+const dombay1 = new URL("../images/dombay-1.jpg", import.meta.url);
+const dombay2 = new URL("../images/dombay-2.jpg", import.meta.url);
+const elbrus1 = new URL("../images/elbrus-1.jpg", import.meta.url);
+const elbrus2 = new URL("../images/elbrus-2.jpg", import.meta.url);
+const karachaevsk = new URL("../images/karachaevsk.jpg", import.meta.url);
+const cherkessiya = new URL("../images/karechaevo-cherkessiya.jpg", import.meta.url);
+
 export const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Домбай',
+    link: dombay1,
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Эверест',
+    link: dombay2,
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Эльбрус',
+    link: elbrus1,
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Фудзи',
+    link: elbrus2,
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Карачаевск',
+    link: karachaevsk,
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Карачаево-Черкессия',
+    link: cherkessiya,
   }
 ];
